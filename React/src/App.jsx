0651a3807fb53af3fcf37335772b7e61ca986307
Rameshwar <rameshwar.swami@mitwpu.edu.in>
2026-05-20 11:17:08 +0530

    React

---
+++ React/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function Title(){
+  return <h1>Hey its me </h1>
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Im Ram</h1>
+        <Title/>
+    </div>
+  )
+
+}
+
+export default App
